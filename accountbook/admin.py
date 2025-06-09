cf7b42648d4d7e3e8a6ff19fb5e18e86b2354292
Chansman <rainsos@nate.com>
2025-06-09 17:05:49 +0900

    ✨ Django ORM 모델 생성 및 Docker 설정 완료
    모델 구현 및 로컬 , Docker 마이그레이트 진행

--- accountbook/admin.py
+++ accountbook/admin.py
@@ -1,20 +1,43 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser, Account, TransactionHistory, Analysis, Notification
+
+from .models import Account, Analysis, CustomUser, Notification, TransactionHistory
+
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ('email', 'nickname', 'name', 'phone_number', 'is_active', 'is_staff', 'last_login')
+    list_display = (
+        'email',
+        'nickname',
+        'name',
+        'phone_number',
+        'is_active',
+        'is_staff',
+        'last_login',
+    )
     search_fields = ('email', 'nickname', 'name')
     ordering = ('-date_joined',)
 
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         ('Personal Info', {'fields': ('nickname', 'name', 'phone_number')}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_admin', 'is_superuser', 'groups', 'user_permissions')}),
+        (
+            'Permissions',
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_admin',
+                    'is_superuser',
+                    'groups',
+                    'user_permissions',
+                )
+            },
+        ),
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
+
 
 @admin.register(Account)
 class AccountAdmin(admin.ModelAdmin):
@@ -22,17 +45,32 @@ class AccountAdmin(admin.ModelAdmin):
     search_fields = ('account_number',)
     list_filter = ('bank_code', 'account_type')
 
+
 @admin.register(TransactionHistory)
 class TransactionHistoryAdmin(admin.ModelAdmin):
-    list_display = ('account', 'transaction_amount', 'transaction_type', 'transaction_method', 'transaction_timestamp')
+    list_display = (
+        'account',
+        'transaction_amount',
+        'transaction_type',
+        'transaction_method',
+        'transaction_timestamp',
+    )
     search_fields = ('transaction_details',)
     list_filter = ('transaction_type', 'transaction_method')
     ordering = ('-transaction_timestamp',)
 
+
 @admin.register(Analysis)
 class AnalysisAdmin(admin.ModelAdmin):
-    list_display = ('user', 'analysis_target', 'analysis_period', 'start_date', 'end_date')
+    list_display = (
+        'user',
+        'analysis_target',
+        'analysis_period',
+        'start_date',
+        'end_date',
+    )
     list_filter = ('analysis_target', 'analysis_period')
+
 
 @admin.register(Notification)
 class NotificationAdmin(admin.ModelAdmin):
