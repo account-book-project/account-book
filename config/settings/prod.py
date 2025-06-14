9332b0059ab1fffe30ab0a1b87424187466de0c0
Chansman <rainsos@nate.com>
2025-06-15 04:09:45 +0900

    🐛 Prod host 권한 설정
    배포 domain만 등록

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,5 +1,5 @@
 from config.settings.base import *
 
 DEBUG = False  # 운영처럼!
-ALLOWED_HOSTS = ["*", "localhost", "127.0.0.1"]  # 로컬/모든 접속 허용
+ALLOWED_HOSTS = ["teamnotfound.duckdns.org"]  # 로컬/모든 접속 허용
 STATIC_ROOT = BASE_DIR / "staticfiles"
