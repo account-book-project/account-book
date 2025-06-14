b5f4508330e23a1db19af2a47ac37dd7bc8dbae7
Chansman <rainsos@nate.com>
2025-06-15 06:13:27 +0900

    🐛 Bug
    bug

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,4 +1,6 @@
+import logging
 from .base import *
+logging.warning(f"[settings] LOADED prod.py → DEBUG={DEBUG}  ALLOWED_HOSTS={ALLOWED_HOSTS}")
 
 DEBUG = False
 ALLOWED_HOSTS = ["teamnotfound.duckdns.org"]
