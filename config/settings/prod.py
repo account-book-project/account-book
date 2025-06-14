3b843496ec27f13ee431e128afcfb7bb7b3244ed
Chansman <rainsos@nate.com>
2025-06-15 05:23:45 +0900

    🐛 Bug
    bug

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -7,7 +7,7 @@ ALLOWED_HOSTS = [
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-CSRF_FAILURE_VIEW = 'django.views.debug.technical_500_response'
-
-CSRF_TRUSTED_ORIGINS = ["https://teamnotfound.duckdns.org"]
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+# CSRF_FAILURE_VIEW = 'django.views.debug.technical_500_response'
+#
+# CSRF_TRUSTED_ORIGINS = ["https://teamnotfound.duckdns.org"]
+# SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
