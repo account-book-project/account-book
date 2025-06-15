a067a2e4574f56cd92aee95b84ce4b66b958a497
Chansman <rainsos@nate.com>
2025-06-15 23:18:49 +0900

    🐛 Templates 적용 불가하게 재설정
    docker-compose 파일에 volumes staticfiles 경로변경

--- config/urls.py
+++ config/urls.py
@@ -30,7 +30,7 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(
             url_name='schema',
             permission_classes=[AllowAny],
-            template_name='custom_swagger_ui.html',  # 여기서 CDN 템플릿 사용
+            # template_name='custom_swagger_ui.html',  # 여기서 CDN 템플릿 사용
         ),
         name='swagger-ui',
     ),
