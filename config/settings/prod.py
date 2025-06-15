2fc1b73ccc621e6b36092e227d8ce658d30408ec
Chansman <rainsos@nate.com>
2025-06-15 22:04:13 +0900

    🐛 Prod host setting
    alloews host 추가 및 수정

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,13 +1,19 @@
 from config.settings.base import *
 
 DEBUG = False
-ALLOWED_HOSTS = ["teamnotfound.duckdns.org"]
+ALLOWED_HOSTS = [
+    "teamnotfound.duckdns.org",
+    "localhost",
+    "127.0.0.1",
+    "nginx",
+    "my-django",
+]
 
 print(f"[settings prod] DEBUG={DEBUG}  ALLOWED_HOSTS={ALLOWED_HOSTS}")
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-# csrf 설정
+# CSRF 설정
 CSRF_TRUSTED_ORIGINS = ["https://teamnotfound.duckdns.org"]
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 CSRF_COOKIE_SECURE = True
